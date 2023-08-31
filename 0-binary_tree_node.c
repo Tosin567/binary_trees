5fdbaed093d8c3ba45567c424ae7f759d8f403f8
Tosin567 <oluwatosinjegede1@gmail.com>
2023-08-31 04:49:06 -0700

    Tosin

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/*typedef struct binary_tree_s {
+    int value;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL) {
+        return NULL; // Failed to allocate memory
+    }
+
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    return new_node;
+}
+
+int main() {
+    // Example usage
+    binary_tree_t *root = binary_tree_node(NULL, 10);
+    binary_tree_t *leftChild = binary_tree_node(root, 5);
+    binary_tree_t *rightChild = binary_tree_node(root, 15);
+
+    root->left = leftChild;
+    root->right = rightChild;
+
+    return 0;
+}
